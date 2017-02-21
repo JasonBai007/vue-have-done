2e8dddd284e1be3b7d05a81bace61c813b0e002b
baiyanyun <baiyy@yiche.com>
2017-02-21 17:06:01 +0800

    add bar

---
+++ app/src/components/Bar.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <el-row :gutter="20" type="flex" justify="center">
+      <el-col :span="4">
+        <div class="grid-content bg-purple">
+          <el-date-picker
+            readonly
+            v-model="date"
+            type="date"
+          >
+          </el-date-picker>
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light">
+          <el-select v-model="project" clearable placeholder="选择项目">
+            <el-option
+              v-for="item in options"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">
+          <el-input placeholder="页面" v-model="page"></el-input>
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple-light">
+          <el-input placeholder="功能" v-model="func"></el-input>
+        </div>
+      </el-col>
+      <el-col :span="2">
+        <div class="grid-content bg-purple">
+          <el-input v-model="percent" placeholder="0">
+            <template slot="append">%</template>
+          </el-input>
+        </div>
+      </el-col>
+      <el-col :span="3">
+        <div class="grid-content bg-purple-light">
+          <el-button type="primary">今天我做了这些</el-button>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'bar',
+  data () {
+    return {      
+      date:moment().format(),
+      project: '',
+      options: [{
+        value: '舆情监控',
+        label: '舆情监控'
+      }, {
+        value: 'AUTODSP-RTB',
+        label: 'AUTODSP-RTB'
+      }],
+      page: '',
+      func: '',
+      percent: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- app/src/main.js
+++ app/src/main.js
@@ -1,11 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Row, Col, Button, Input, Select, Option, DatePicker } from 'element-ui'
 import App from './App'
 import router from './router'
 
+Vue.component(Row.name, Row)
+Vue.component(Col.name, Col)
 Vue.component(Button.name, Button)
+Vue.component(Input.name, Input)
+Vue.component(Select.name, Select)
+Vue.component(Option.name, Option)
+Vue.component(DatePicker.name, DatePicker)
 
 /* eslint-disable no-new */
 new Vue({
